f32736ac4cae33131efb3e43437441a20a37c001
Caue Almeida <nncl@live.com>
2026-05-02 13:14:29 -0300

    ci: Mapeia admin do Django em nova rota

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/", include("portfolio.urls_public")),
     path("api/", include("contacts.urls_public")),
